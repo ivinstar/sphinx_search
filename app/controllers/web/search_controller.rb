6f095f0581a8df2a1ae60f696d4762632b83a783
Tatiana Podymova <tatyana.podymova@simbirsoft.com>
2016-02-01 17:26:49 +0300

    search engine

--- app/controllers/web/search_controller.rb
+++ app/controllers/web/search_controller.rb
@@ -1,13 +1,20 @@
 class Web::SearchController < Web::ApplicationController
 
   def index
-  	q = params.try(:[],:search).try(:[],:text)
-  	if q.nil?
+  	@q = params.try(:[],:search).try(:[],:text)
+  	if @q.nil?
   	  @collection = {}	
   	else
-  	  @collection = Article.search  q	
+  	  @collection = Article.search  @q	
   	end	
   	
   end	
+
+  
+  def show
+  	@word = params[:word]
+  	@collection = Article.find(params[:id])
+  end	
+
 
 end	
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ActiveRecord::Base
+
+
+  def date
+  	self.updated_at.utc.strftime('%d.%m.%Y')
+  end	
+
+  def short
+  	"#{self.content[0..300]}..."
+  end	
+
+  def self.colorize field, string
+  	field.gsub(/#{string}/i) {|s| "<div class='text-success'>#{string}</div>"}
+  end	
+
+
 end
